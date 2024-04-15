a2cf92a375510cf9a9d94517476822591bda165a
balazs burian <bur.balazs@protonmail.com>
2024-04-15 19:50:03 +0200

    firebase set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import './App.css'
 import { useState, useEffect, useRef } from 'react'
 import Die from './components/Die'
-// import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
 import Timer from './components/Timer'
-// import {useWindowSize} from 'react'
+import UserName from './components/UserName'
+import Results from './components/Results'
 
+import Confetti from 'react-confetti'
 
-
-import './App.css'
+// fireBase
+import { db, playerRanking } from './firebase.js'
+import { onSnapshot, addDoc, setDoc, doc  } from "firebase/firestore"
+// import playerRanking from './firebase.js'
 
 function App() {
 
@@ -19,63 +22,15 @@ function App() {
   const [isRunning, setIsRunning] = useState(false)
 
 
-  // TIMER ON
-  // the starting function is in the rollDice function. I used tutorials for this.
-  // But documented here, to be understood.
+  const [userName, setUserName] = useState('')
+
+  const [results, setResults] = useState([
+
+  ])
 
 
-  let timeInterval = useRef(null)
 
-  function starter() {
-    // Checks if the timer is running.
-    if (isRunning) return;
-    // If not then sets the timer to true and will run.
-    setIsRunning(true);
-    // it updates the interval by 10ms incrementing the previous value.
-    timeInterval.current = setInterval(() => {
-      // sets our state to the new value.
-      setTimer((prev) => prev + 1)
-    }, 10)
-  }
-
-
-  function pause() {
-    // Checks if the timer is false. If it is not running returns and doesn't do anything.
-    if (!isRunning) return;
-    // if the timer is running then it sets the timner to false and pauses.
-    setIsRunning(false);
-    // Stops the timer from incrementing.
-    clearInterval(timeInterval.current);
-  }
-
-  function reset() {
-    // Sets the timer to false. This happens automatically when the game is over
-    setIsRunning(false)
-    // Stops the timer from incrementing.
-    clearInterval(timeInterval.current)
-    // resets the timer state to 0. For the next game
-    setTimer(0)
-  }
-
-// TIMER OFF
-
-  function populate() {
-    let dice = []
-    for (let i = 0; i < 10; i++) {
-      let random = Math.floor((Math.random() * 6) + 1)
-      let die = {
-        key: i,
-        value: '',
-        isHeld: false,
-        id: i
-      }
-      dice.push(die)
-    }
-    return dice
-  }
-
-
-  useEffect(() => {
+useEffect(() => {
     const reference = numbers[0].value
 
     let counter = 0
@@ -93,16 +48,84 @@ function App() {
   }, [numbers])
 
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(playerRanking, function(snapshot) {
+
+      const resultArray = snapshot.docs.map(doc => {
+        return {
+          ...doc.data(),
+          id: doc.id
+        }
+      })
+      setResults(resultArray)
+    })
+    return unsubscribe
+  }, [])
+
+
+
+  function populate() {
+    let dice = []
+    for (let i = 0; i < 10; i++) {
+      // let random = Math.floor((Math.random() * 6) + 1)
+      let die = {
+        key: i,
+        value: '',
+        isHeld: false,
+        id: i
+      }
+      dice.push(die)
+    }
+    return dice
+  }
   // I did not need this function, because I added a populate function, which populates the array with the dice without value
   //  the value will be added to the dice when the user clicks to Roll
 
   // function createDice() {
 
-  //   let dice = []
+    // TIMER ON
+    // the starting function is in the rollDice function. I used tutorials for this.
+    // But documented here, to be understood.
+
+
+    let timeInterval = useRef(null)
+
+    function starter() {
+      // Checks if the timer is running.
+      if (isRunning) return;
+      // If not then sets the timer to true and will run.
+      setIsRunning(true);
+      // it updates the interval by 10ms incrementing the previous value.
+      timeInterval.current = setInterval(() => {
+        // sets our state to the new value.
+        setTimer((prev) => prev + 1)
+      }, 10)
+    }
+
+    function pause() {
+      // Checks if the timer is false. If it is not running returns and doesn't do anything.
+      if (!isRunning) return;
+      // if the timer is running then it sets the timner to false and pauses.
+      setIsRunning(false);
+      // Stops the timer from incrementing.
+      clearInterval(timeInterval.current);
+    }
+
+    function reset() {
+      // Sets the timer to false. This happens automatically when the game is over
+      setIsRunning(false)
+      // Stops the timer from incrementing.
+      clearInterval(timeInterval.current)
+      // resets the timer state to 0. For the next game
+      setTimer(0)
+    }
+
+  // TIMER OFF
+    //   let dice = []
   //   for (let i = 0; i < 10; i++) {
   //     let random = Math.floor((Math.random() * 6) + 1)
   //     let die = {
-  //       key: i,
+    //       key: i,
   //       value: random,
   //       isHeld: false,
   //       id: i
@@ -143,12 +166,49 @@ function App() {
         setDraw(prev => prev + 1)
 
       } else {
+
         setTenzies(false)
         setNumbers(populate())
         setDraw(0)
         reset()
       }
     }
+
+
+    function handleChange(event) {
+      setUserName(event.target.value)
+    }
+
+
+    async function sendResults(event) {
+        // const id = results[0].id
+        // console.log(id)
+        // const docRef = doc(db, 'ranklist', )
+        event.target.disabled = true
+        event.target.innerText = 'Results sent'
+
+        const result =  {
+          userName: userName ? userName : "no username",
+          time: timer,
+          draw: draw
+          }
+      await addDoc(playerRanking, result)
+
+      }
+
+
+      function getFastest() {
+        const fastest = results.sort((a, b) => a.time - b.time)
+        return fastest.slice(0, 10)
+      }
+
+      function getLeastDraw() {
+        const leastDraw = results.sort((a, b) => a.draw - b.draw)
+        return leastDraw.slice(0, 10)
+      }
+
+
+
 
 
 
@@ -164,6 +224,9 @@ function App() {
          <p className="counter">{draw <= 0 ? "Let's play!" :  `You rolled ${draw} times.` }</p>
             <button className="roll" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
          <Timer timer={timer} ></Timer>
+         {tenzies && <UserName handleUserName={handleChange} userName={userName} sendResults={sendResults}></UserName>}
+         {tenzies && <Results fastestResult={getFastest()} drawResult={getLeastDraw()}></Results> }
+
       </main>
     </>
   )
